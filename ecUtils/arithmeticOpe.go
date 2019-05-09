e89b846f6a488cbb55f6ca4c20dca8d312eebe46
ykm11 <ushiromiya3@gmail.com>
2019-05-09 17:42:03 +0900

    impl Tonelli–Shanks algorithm

--- ecUtils/arithmeticOpe.go
+++ ecUtils/arithmeticOpe.go
@@ -95,3 +95,37 @@ func CRT(remainders, modulus []*big.Int) *big.Int {
     return mod(x, N)
 }
 
+func Legendre(x, p *big.Int) bool { // is x a quadratic-residue?
+    r := exp(x, div(sub(p, ONE, p), TWO), p)
+    return r.Cmp(ONE) == 0
+}
+
+func GetQuadraticResidueRoot(x, modulus *big.Int) *big.Int {
+    if !Legendre(x, modulus) {
+        return nil
+    }
+    z := ONE
+    for ; exp(z, div(sub(modulus, ONE, nil), TWO), modulus).Cmp(sub(modulus, ONE, nil)) != 0; {
+        z = add(z, ONE, nil)
+    }
+    q := sub(modulus, ONE, nil)
+
+    m := ZERO
+    for ; mod(q, TWO).Cmp(ZERO) == 0; {
+        q = div(q, TWO)
+        m = add(m, ONE, nil)
+    }
+
+    c := exp(z, q, modulus)
+    t := exp(x, q, modulus)
+    r := exp(x, div(add(q, ONE, nil), TWO), modulus)
+    for i := ValCopy(m); i.Cmp(ONE) != 0; i = sub(i, ONE, nil) {
+        tmp := exp(t, exp(TWO, sub(i, TWO, nil), nil), modulus)
+        if tmp.Cmp(ONE) != 0 {
+            r = mul(r, c, modulus)
+            t = mul(t, exp(c, TWO, modulus), modulus)
+        }
+        c = exp(c, TWO, modulus)
+    }
+    return r
+}
